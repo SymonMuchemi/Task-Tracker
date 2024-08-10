ec55199ea5eb16e2f8f68929a3ab9a408170add0
symonmuchemi <simonkenya846@gmail.com>
2024-08-10 17:07:12 +0300

    feat: creates storage class
    adds object to storage, save to file and retrieve all session stored objects

--- models/storage.py
+++ models/storage.py
@@ -1,2 +1,41 @@
 #!/usr/bin/python3
 """ storage module """
+import json
+
+
+class File_storage:
+    __file_path = 'tasks.json'
+    __objects = {}
+    
+    def all(self):  
+        """returns all the objects stored in the objects dictionary
+
+        Returns:
+            dict: dictionary of objects
+        """
+        return {
+            key: val for key, val in File_storage.__objects.items()
+        }
+
+    def new(self, obj):
+        """adds a new object to the objects dict
+
+        Args:
+            obj (dict): object to be added
+        """
+        if obj is not None:
+            File_storage.__objects.all().update(
+                {
+                    obj.to_dict()['description']: obj
+                }
+            )
+
+    def save(self):
+        """save storage to file"""
+        with open(self.__file_path, "w") as file:
+            temp_dict = {}
+            temp_dict.update(File_storage.__objects)
+            for key, val in temp_dict:
+                temp_dict[key] = val.to_dict()
+            json.dump(temp_dict)
+            
