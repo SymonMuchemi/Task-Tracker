e9089875448de57f37f14034c5b2112fb6e1498a
symonmuchemi <simonkenya846@gmail.com>
2024-08-13 01:03:11 +0300

    feat: implements the listing functionality

--- console.py
+++ console.py
@@ -30,6 +30,17 @@ class TaskCLI(cmd.Cmd):
             storage.save()
             print(f"Task saved with id: {new_task.id}")
 
+    def do_list(self, args):
+        """list tasks """
+        n = len(args)
+        print(args if n == 0 else '')
+        if n == 0:
+            all_tasks = storage.all()
+            print(len(all_tasks))
+            print(type(all_tasks))
+            print("ID  Status\t Description")
+            for key, task in all_tasks.items():
+                print(task)
 
 if __name__ == '__main__':
     TaskCLI().cmdloop()
