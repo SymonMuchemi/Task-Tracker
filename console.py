8713dcaec5ea0095b5256757b3b656d36cb2ab9c
symonmuchemi <simonkenya846@gmail.com>
2024-08-13 19:11:23 +0300

    update: added quit method and refatored list method

--- console.py
+++ console.py
@@ -5,6 +5,8 @@ import sys
 from models.task import Task
 from models import storage
 
+states = ['not-done', 'done', 'in-progress']
+
 
 class TaskCLI(cmd.Cmd):
     prompt = 'task-cli '
@@ -19,28 +21,58 @@ class TaskCLI(cmd.Cmd):
         storage.save()
         return True
 
+    def do_quit(self):
+        """exit the console application"""
+        storage.save()
+        print("Exiting the console")
+        return True
+
     def do_add(self, args):
         """ Adds a new task """
         desc = args.replace('"', '')
 
-        if len(args) >= 2:
+        if len(desc) >= 2:
             new_task = Task(description=desc)
-            storage.reload()
             storage.new(new_task)
             storage.save()
             print(f"Task saved with id: {new_task.id}")
+        else:
+            print("Usage: add <task description>")
 
     def do_list(self, args):
-        """list tasks """
-        n = len(args)
-        print(args if n == 0 else '')
-        if n == 0:
-            all_tasks = storage.all()
-            print(len(all_tasks))
-            print(type(all_tasks))
-            print("ID  Status\t Description")
-            for key, task in all_tasks.items():
-                print(task)
+        """list tasks
+        usage: list
+        """
+        all_tasks = storage.all()
+        print("ID Status Description")
+        for task in all_tasks.values():
+            print(task)
+
+    def do_mark(self, args):
+        """Changes the status of a task"""
+        args = args.split()
+        print(args)
+        task = None
+        id = args[0] or None
+        new_status = args[1].lower() if args[1] else None
+        
+        if new_status is None:
+            print("Status cannot be empty")
+
+        if len(args) == 2:
+            tasks = storage.all()
+            print(val for val in tasks.values())
+            if id in tasks:
+                task = tasks[id]
+                print(type(task))
+            else:
+                print("Task not found")
+            
+            print(task)
+            task.update(status=new_status)
+            print(task)
+            storage.save()
+
 
 if __name__ == '__main__':
     TaskCLI().cmdloop()
